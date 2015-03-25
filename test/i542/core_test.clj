7eae9e7904fba3166e7a0ff40db9cb74394a73c4
Jim Crossley <jim@crossleys.org>
2015-03-25 17:50:44 -0400

    Add a listener test that I've yet to see hang

--- test/i542/core_test.clj
+++ test/i542/core_test.clj
@@ -24,3 +24,19 @@
                      t2 (System/currentTimeMillis)]
                  (println (format "Got %s (%s ms)" m (- t2 t1)))
                  m)))))))
+
+(deftest publish-and-listen
+  (with-open [ctx1 (msg/context (assoc opts :port (offset-port :http)))]
+    (let [q1 (msg/queue "/queue/i542" :context ctx1)
+          received (atom [])
+          p (promise)
+          c 100]
+      (dotimes [i c]
+        (msg/publish q1 i))
+      (msg/listen q1 (fn [m]
+                       (println "GOT:" m)
+                       (swap! received conj m)
+                       (when (= c (count @received))
+                         (deliver p :done))))
+      (deref p 60000 nil)
+      (is (= (range c) @received)))))
